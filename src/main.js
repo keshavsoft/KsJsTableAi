d6600c02e7b8fbe5656835e97bd4dda785a0c5e7
keshavsoft <nknnkumar@yahoo.com>
2026-05-08 19:01:33 +0530

    v29 added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { initTable, initVertical, initTableOnly } from "./KSTableAi/V25/entry.js";
+import { initTable, initVertical, initTableOnly } from "./KSTableAi/V29/entry.js";
 import { initHeader } from "./header/v1/initHeader.js";
 
 window.KSTable = { initTable, initVertical, initTableOnly };
