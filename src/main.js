ecfac3e96c89d8f52fe9f43ce0fa993759857f98
keshavsoft <nknnkumar@yahoo.com>
2026-05-19 18:56:47 +0530

    42 search perfect

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // import { initTable, initVertical, initTableOnly, initShowTable } from "./KSTableAi/V41/entry.js";
-import { initShowTable } from "./KSTableAi/V41/entry.js";
+import { initShowTable } from "./KSTableAi/V42/entry.js";
 
 import { initHeader } from "./header/v5/initHeader.js";
 
