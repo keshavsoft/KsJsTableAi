24d75bb6f17eaaf5bc50b5721d0a0cc852a8f1d5
keshavsoft <nknnkumar@yahoo.com>
2026-05-19 16:27:26 +0530

    on show table

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { initTable, initVertical, initTableOnly, initShowTable } from "./KSTableAi/V41/entry.js";
+// import { initTable, initVertical, initTableOnly, initShowTable } from "./KSTableAi/V41/entry.js";
+import { initShowTable } from "./KSTableAi/V41/entry.js";
+
 import { initHeader } from "./header/v5/initHeader.js";
 
 window.KSTable = { initTable, initVertical, initTableOnly, initShowTable };
